969d253c90078cc2e5d682f01d13e2537571b78c
Ry-an7 <117701680+Ry-an7@users.noreply.github.com>
2024-11-30 20:08:04 -0800

    finished adding all vis in d3

--- vis.js
+++ vis.js
@@ -379,9 +379,335 @@ async function drawVis6() {
     });
 }
 
+async function drawVis7() {
+
+    const margin = {top: 20, right: 30, bottom: 50, left: 60},
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+    const tickValues = d3.range(8, 12.9, 0.5).concat(12.9);
+
+        const svg = d3.select("#vis3-1")
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        d3.csv("datasets/LeagueTotals.csv").then(data => {
+            
+        const cleanedData = data
+            .filter(d => d["Season"] && d["Shooting Percentage"]) 
+            .map(d => ({
+                Season: +d["Season"].split('-')[0], 
+                ShootingPercentage: +d["Shooting Percentage"]
+            }))
+            
+            .sort((a, b) => a.Season - b.Season);
+            console.log(cleanedData);
+        
+        const x = d3.scaleLinear()
+            .domain(d3.extent(cleanedData, d => d.Season))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([8, d3.max(cleanedData, d => d.ShootingPercentage)])
+            .range([height, 0]);
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", 40)
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Season");
+
+        svg.append("g")
+            .call(d3.axisLeft(y).tickValues(tickValues))
+            .append("text")
+            .attr("x", -height / 2)
+            .attr("y", -50)
+            .attr("transform", "rotate(-90)")
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Shooting Percentage");
+
+        const line = d3.line()
+            .x(d => x(d.Season))
+            .y(d => y(d.ShootingPercentage));
+
+        svg.append("path")
+            .datum(cleanedData)
+            .attr("class", "line")
+            .attr("d", line);
+    });
+}
+
+async function drawVis8() {
+
+    const margin = {top: 20, right: 30, bottom: 50, left: 60},
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+    const tickValues = d3.range(8.5, 12.2, 0.5).concat(12.2);
+
+        const svg = d3.select("#vis3-2")
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        d3.csv("datasets/LeagueTotals.csv").then(data => {
+            const cleanedData = data
+                .filter(d => d["Season"] && d["Shooting Percentage"]) 
+                .map(d => ({
+                    Season: +d["Season"].split('-')[0], 
+                    ShootingPercentage: +d["Shooting Percentage"]
+            }))
+            .filter(d => d.Season >= 1989 && d.Season <= 2024)
+            .sort((a, b) => a.Season - b.Season);
+            console.log(cleanedData);
+        
+        const x = d3.scaleLinear()
+            .domain(d3.extent(cleanedData, d => d.Season))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([8.5, d3.max(cleanedData, d => d.ShootingPercentage)])
+            .range([height, 0]);
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", 40)
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Season");
+
+        svg.append("g")
+            .call(d3.axisLeft(y).tickValues(tickValues))
+            .append("text")
+            .attr("x", -height / 2)
+            .attr("y", -50)
+            .attr("transform", "rotate(-90)")
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Shooting Percentage");
+
+        const line = d3.line()
+            .x(d => x(d.Season))
+            .y(d => y(d.ShootingPercentage));
+
+        svg.append("path")
+            .datum(cleanedData)
+            .attr("class", "line")
+            .attr("d", line);
+    });
+}
+
+async function drawVis9() {
+
+    const margin = { top: 20, right: 30, bottom: 50, left: 60 },
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+    const tickValues = d3.range(0.865, 0.930, 0.005);
+
+    const svg = d3.select("#vis4-1")
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    d3.csv("datasets/LeagueTotals.csv").then(data => {
+        const cleanedData = data
+            .filter(d => d["Season"] && d["Save Percentage"])
+            .map(d => ({
+                Season: +d["Season"].split('-')[0],
+                SavePercentage: +d["Save Percentage"]
+        }))
+        .sort((a, b) => a.Season - b.Season);
+        console.log(cleanedData);
+
+        const x = d3.scaleLinear()
+            .domain(d3.extent(cleanedData, d => d.Season))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([0.865, 0.925])
+            .range([height, 0]);
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", 40)
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Season");
+
+        svg.append("g")
+            .call(d3.axisLeft(y).tickValues(tickValues))
+            .append("text")
+            .attr("x", -height / 2)
+            .attr("y", -50)
+            .attr("transform", "rotate(-90)")
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Save Percentage");
+
+        const line = d3.line()
+            .x(d => x(d.Season))
+            .y(d => y(d.SavePercentage));
+
+        svg.append("path")
+            .datum(cleanedData)
+            .attr("class", "line")
+            .attr("d", line);
+    });
+}
+
+async function drawVis10() {
+
+    const margin = { top: 20, right: 30, bottom: 50, left: 60 },
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+    const tickValues = d3.range(0.890, 0.915, 0.001);
+
+    const svg = d3.select("#vis4-2")
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    d3.csv("datasets/LeagueTotals.csv").then(data => {
+        const cleanedData = data
+            .filter(d => d["Season"] && d["Save Percentage"])
+            .map(d => ({
+                Season: +d["Season"].split('-')[0],
+                SavePercentage: +d["Save Percentage"]
+            }))
+            .filter(d => d.Season >= 1995 && d.Season <= 2005)
+            .sort((a, b) => a.Season - b.Season);
+            console.log(cleanedData);
+
+        const x = d3.scaleLinear()
+            .domain(d3.extent(cleanedData, d => d.Season))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([0.890, 0.915])
+            .range([height, 0]);
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", 40)
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Season");
+
+        svg.append("g")
+            .call(d3.axisLeft(y).tickValues(tickValues))
+            .append("text")
+            .attr("x", -height / 2)
+            .attr("y", -50)
+            .attr("transform", "rotate(-90)")
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Save Percentage");
+
+        const line = d3.line()
+            .x(d => x(d.Season))
+            .y(d => y(d.SavePercentage));
+
+        svg.append("path")
+            .datum(cleanedData)
+            .attr("class", "line")
+            .attr("d", line);
+    });
+}
+
+async function drawVis11() {
+
+    
+    const margin = {top: 20, right: 30, bottom: 50, left: 60},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+        const svg = d3.select("#vis5-1")
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        d3.csv("datasets/LeagueTotals.csv").then(data => {
+            
+        const cleanedData = data
+            .filter(d => d["Season"] && d["Number of Teams"]) 
+            .map(d => ({
+                Season: +d["Season"].split('-')[0], 
+                NumberofTeams: +d["Number of Teams"]
+            }))
+            .filter(d => d.Season >= 1979 && d.Season <= 2006)
+            .sort((a, b) => a.Season - b.Season);
+            console.log(cleanedData);
+        
+        const x = d3.scaleLinear()
+            .domain(d3.extent(cleanedData, d => d.Season))
+            .range([0, width]);
+
+        const y = d3.scaleLinear()
+            .domain([20, d3.max(cleanedData, d => d.NumberofTeams)])
+            .range([height, 0]);
+
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+            .append("text")
+            .attr("x", width / 2)
+            .attr("y", 40)
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Season");
+
+        svg.append("g")
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("x", -height / 2)
+            .attr("y", -50)
+            .attr("transform", "rotate(-90)")
+            .attr("fill", "black")
+            .style("font-size", "14px")
+            .style("text-anchor", "middle")
+            .text("Number of Teams");
+
+        const line = d3.line()
+            .x(d => x(d.Season))
+            .y(d => y(d.NumberofTeams));
+
+        svg.append("path")
+            .datum(cleanedData)
+            .attr("class", "line")
+            .attr("d", line);
+    });
+}
+
+
+
     drawVis1();
     drawVis2();
     drawVis3();
     drawVis4();
     drawVis5();
     drawVis6();
+    drawVis7();
+    drawVis8();
+    drawVis9();
+    drawVis10();
+    drawVis11();
